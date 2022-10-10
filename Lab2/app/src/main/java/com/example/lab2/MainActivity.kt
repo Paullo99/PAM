c7f012675624c3a0d1480f2ceef9cc1cf3f982be
Paweł Maciończyk <macionczyk.paw@gmail.com>
2022-10-10 20:01:59 +0200

    add overrided functions and create Button "Exit"

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -2,17 +2,56 @@ package com.example.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private val duration = Toast.LENGTH_SHORT;
+    private val duration = Toast.LENGTH_SHORT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "onCreate", duration).show();
+        Toast.makeText(this, "onCreate", duration).show()
+
+        val buttonExit = findViewById<Button>(R.id.buttonExit)
+        buttonExit.setOnClickListener {
+            finish()
+        }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", duration).show()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", duration).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", duration).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", duration).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", duration).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", duration).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Toast.makeText(this, "onSaveInstanceState", duration).show()
+    }
 }
